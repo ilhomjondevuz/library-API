ee3617802de13cb10afcc945b9343697c099445b
Ilhomjon <rahimovilhomjon25@gmail.com>
2026-04-06 18:50:38 +0500

    Many changes

--- books/serializers.py
+++ books/serializers.py
@@ -7,3 +7,13 @@ class BookSerializer(serializers.ModelSerializer):
     class Meta:
         model = Book
         fields = ('id', 'title', 'description', 'author','price', 'image', 'created_at', 'updated_at')
+
+    def validate(self, data):
+        title = data.get('title', None)
+        author = data.get('author', None)
+        book = Book.objects.filter(title=title, author=author).exists()
+        if title.isdigit() or title.isnumeric():
+            raise serializers.ValidationError('Book title is invalid')
+        if book:
+            raise serializers.ValidationError('Book already exists')
+        return data
--- books/views.py
+++ books/views.py
@@ -1,8 +1,11 @@
+from drf_spectacular.utils import extend_schema
 from rest_framework import generics, status
 from rest_framework.decorators import api_view, permission_classes
+from rest_framework.generics import get_object_or_404
 from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
+# from rest_framework.generics import ListCreateAPIView, RetrieveDestroyAPIView, RetrieveUpdateAPIView
 
 from .models import Book
 
@@ -17,10 +20,10 @@ class BookListAPIView(APIView):
 
     def get(self, request, *args, **kwargs):
         books = Book.objects.all()
-        serializer = BookSerializer(books, many=True)
+        serializer_data = BookSerializer(books, many=True).data
         data = {
             'response': f"Returen: {len(books)}",
-            'books': serializer.data,
+            'books': serializer_data,
             'status': status.HTTP_200_OK,
         }
         return Response(data)
@@ -42,15 +45,52 @@ class BookDeleteAPIView(APIView):
         book.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
 
-class BookUpdateAPIView(generics.UpdateAPIView):
-    permission_classes = [AllowAny]
-    queryset = Book.objects.all()
-    serializer_class = BookSerializer
+# class BookUpdateAPIView(generics.UpdateAPIView):
+#     permission_classes = [AllowAny]
+#     queryset = Book.objects.all()
+#     serializer_class = BookSerializer
 
-class BookCreateAPIView(generics.CreateAPIView):
+@extend_schema(
+    request=BookSerializer,
+    responses={
+        'success': True,
+        'message': 'Book created successfully',
+        'data': BookSerializer,
+    }
+)
+class BookUpdateAPIView(APIView):
     permission_classes = [AllowAny]
-    queryset = Book.objects.all()
-    serializer_class = BookSerializer
+    def put(self, request, *args, **kwargs):
+        book = get_object_or_404(Book, pk=kwargs['pk'])
+        serializer = BookSerializer(instance=book, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            response = {
+                'success': True,
+                'message': 'Book updated successfully',
+                'data': serializer.data,
+            }
+            return Response(response, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+# class BookCreateAPIView(generics.CreateAPIView):
+#     permission_classes = [AllowAny]
+#     queryset = Book.objects.all()
+#     serializer_class = BookSerializer
+
+@extend_schema(
+    request=BookSerializer,
+    responses=BookSerializer
+)
+class BookCreateAPIView(APIView):
+    permission_classes = [AllowAny]
+    def post(self, request, *args, **kwargs):
+        serializer = BookSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 @api_view(['GET'])
@@ -60,6 +100,23 @@ def book_list(request, *args, **kwargs):
     serializer = BookSerializer(books, many=True)
     return Response(serializer.data)
 
-class BookRetrieveDeleteAPIView(generics.RetrieveDestroyAPIView):
-    queryset = Book.objects.all()
-    serializer_class = BookSerializer
+# class BookRetrieveDeleteAPIView(generics.RetrieveDestroyAPIView):
+#     queryset = Book.objects.all()
+#     serializer_class = BookSerializer
+
+class BookRetrieveDeleteAPIView(APIView):
+    permission_classes = [AllowAny]
+    def get(self, request, *args, **kwargs):
+        try:
+            book = Book.objects.get(pk=kwargs['pk'])
+            serializer = BookSerializer(book)
+            return Response(serializer.data)
+        except Book.DoesNotExist as e:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+    def delete(self, request, *args, **kwargs):
+        try:
+            book = Book.objects.get(pk=kwargs['pk'])
+            book.delete()
+            return Response(status=status.HTTP_204_NO_CONTENT)
+        except Book.DoesNotExist as e:
+            return Response(status=status.HTTP_404_NOT_FOUND)
